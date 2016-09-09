f218255b18505700808a43f7bdc29c9986b05167
Michael Teter <michael.teter@gmail.com>
2016-09-09 22:23:07 +0200

    first commit

---
+++ AF3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AF3
+//
+//  Created by Michael Teter on 2016-09-09.
+//  Copyright © 2016 Michael Teter. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AF3' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AF3
+  pod 'Alamofire',
+    :git => 'https://github.com/Alamofire/Alamofire.git',
+    :branch => 'swift3-rebased'
+end
